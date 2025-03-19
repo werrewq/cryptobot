d625f9c6a08bde668d1fddcb1a6169cb25c3220d
werrewq <werrewq@ya.ru>
2025-03-19 18:11:26 +0300

    fixed ports 3

--- Test/TestSignalRequest.py
+++ Test/TestSignalRequest.py
@@ -2,6 +2,7 @@ import requests
 
 #url = 'http://localhost:8000/position'
 url = 'http://77.232.135.88/position'
+#url = 'http://werrewq-cryptobot-719a.twc1.net/position'
 
 #"{"signal":"{{strategy.order.comment}}","token":"2hiKjBiVGL5LkkBKObXmQA6h4GoedZ5CYyQ7F8bOO12GES9pdTsisADIdcXUjTF2"}"
 
