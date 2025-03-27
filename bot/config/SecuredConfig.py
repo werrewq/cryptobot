04a89bf23743b7a45f295b7af755d7be245c18f7
werrewq <werrewq@ya.ru>
2025-03-27 19:15:52 +0300

    env fix 2

--- bot/config/SecuredConfig.py
+++ bot/config/SecuredConfig.py
@@ -10,10 +10,6 @@ test_broker_api_key = "ZcNKoTlZYkouIPeXGE"
 test_broker_secret_key = "3EEwQjfzXwyOh72bHjQEB7prHQEBODIk044E"
 test_telegram_bot_api_token = "7848584263:AAH2EY10kySewTnclRLiQf6T9LPoae_yJnk"
 test_cryptobot_api_token = "2hiKjBiVGL5LkkBKObXmQA6h4GoedZ5CYyQ7F8bOO12GES9pdTsisADIdcXUjTF2"
-# $env:BROKER_API_KEY = "ZcNKoTlZYkouIPeXGE"
-# $env:BROKER_SECRET_KEY = "3EEwQjfzXwyOh72bHjQEB7prHQEBODIk044E"
-# $env:TELEGRAM_BOT_API_TOKEN = "7848584263:AAH2EY10kySewTnclRLiQf6T9LPoae_yJnk"
-# $env:CRYPTOBOT_API_TOKEN = "2hiKjBiVGL5LkkBKObXmQA6h4GoedZ5CYyQ7F8bOO12GES9pdTsisADIdcXUjTF2"
 
 # only server
 # test_broker_api_key ZcNKoTlZYkouIPeXGE
