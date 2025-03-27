9df16a1133935b9383376662b4ce9c5c33a599ae
werrewq <werrewq@ya.ru>
2025-03-27 21:03:23 +0300

    env fix 3

--- bot/config/SecuredConfig.py
+++ bot/config/SecuredConfig.py
@@ -3,13 +3,14 @@ import os
 
 from bot.domain.dto.TradingConfig import TradingConfig
 
-# Тестовые ключи
-# test_broker_api_key = "6pAf7l2HZn46GqJqu6"
-# test_broker_secret_key = "FHfaEudS6euKkiVobB6cDTkDzXs6TIBhX9Iu"
 test_broker_api_key = "ZcNKoTlZYkouIPeXGE"
 test_broker_secret_key = "3EEwQjfzXwyOh72bHjQEB7prHQEBODIk044E"
 test_telegram_bot_api_token = "7848584263:AAH2EY10kySewTnclRLiQf6T9LPoae_yJnk"
 test_cryptobot_api_token = "2hiKjBiVGL5LkkBKObXmQA6h4GoedZ5CYyQ7F8bOO12GES9pdTsisADIdcXUjTF2"
+
+# Тестовые ключи
+# test_broker_api_key = "6pAf7l2HZn46GqJqu6"
+# test_broker_secret_key = "FHfaEudS6euKkiVobB6cDTkDzXs6TIBhX9Iu"
 
 # only server
 # test_broker_api_key ZcNKoTlZYkouIPeXGE
