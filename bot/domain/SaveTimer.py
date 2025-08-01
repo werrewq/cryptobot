90c395976c08e57e6761f28a7e12efb3560e8bd2
werrewq <werrewq@ya.ru>
2025-08-01 13:56:21 +0300

    Timer auction open/end fix

--- bot/domain/SaveTimer.py
+++ bot/domain/SaveTimer.py
@@ -19,12 +19,16 @@ class SaveTimer:
         self._start_time = datetime.time(start_hour, start_minute)
         self._end_time = datetime.time(end_hour, end_minute)
 
+        # TODO вынести в конфиг
+        self._block_window_start_time = datetime.time(18, 40)
+        self._block_window_end_time = datetime.time(19, 5)
+
     def _is_work_time(self):
         now = datetime.datetime.now(self._tz)
         if now.weekday() > 4:
             return False
         current_time = now.time()
-        return self._start_time <= current_time <= self._end_time
+        return self._start_time <= current_time <= self._end_time and not (self._block_window_start_time <= current_time <= self._block_window_end_time)
 
     def _run(self):
         with self._lock:
@@ -50,6 +54,8 @@ class SaveTimer:
             self._timer.start()
 
     def reset_timer(self):
+        if not self._is_work_time():
+            return
         with self._lock:
             if self._timer:
                 self._timer.cancel()
